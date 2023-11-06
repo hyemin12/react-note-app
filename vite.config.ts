106c10504f572388ea6dcc1b8a35e784037be68c
hyemin12 <khnm31@naver.com>
2023-11-06 14:07:00 +0900

    docs: 기본 구조 잡기, 절대경로 별칭 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@pages", replacement: path.resolve(__dirname, "src.pages") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      { find: "@store", replacement: path.resolve(__dirname, "src/store") },
+      { find: "@types", replacement: path.resolve(__dirname, "src/types") },
+      {
+        find: "@styles",
+        replacement: path.resolve(__dirname, "src/assets/styles"),
+      },
+    ],
+  },
+});
