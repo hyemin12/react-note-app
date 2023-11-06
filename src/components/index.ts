60074a7dc4841c118e29482c68275b9d341d95ee
hyemin12 <khnm31@naver.com>
2023-11-06 14:27:45 +0900

    docs: 기본 구조 설정 및 라우터 적용

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as TagsModal } from "./Modal/TagsModal/TagsModal";
+export { default as CreateNoteModal } from "./Modal/CreateNoteModal/CreateNoteModal";
+export { default as ReadNoteModal } from "./Modal/ReadNoteModal/ReadNoteModal";
+export { default as FilterModal } from "./Modal/FilterModal/FilterModal";
+
+export { default as TextEditor } from "./TextEditor/TextEditor";
+export { default as NoteCard } from "./NoteCard/NoteCard";
+export { default as MainWrapper } from "./MainWrapper/MainWrapper";
